6f81c0af369dd6d43d555806004b310ad4922380
GaneshmKumar <ganeshkumar.m1996@gmail.com>
2017-06-10 20:47:05 +0530

    Changed UI

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculator
+A JavaScript Calculator
+
+# Dependencies
+* Bootstrap
+* Jquery
+
+# Preview
+<a href="https://ganeshmkumar.github.io/Calculator">Calculator</a>
---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+* @Author: GaNeShKuMaRm
+* @Date:   2017-05-14 19:01:19
+* @Last Modified by:   GaNeShKuMaRm
+* @Last Modified time: 2017-05-14 19:01:35
+*/
+
+'use strict';
+$(document).ready(function() {
+  var expression = "";
+  $(".buttons div").on('click', function() {
+    //alert(expression);
+    var key = $(this).text();
+    if(key == 'AC')
+    {
+      $('input').val("");
+    }
+    else if(key == 'CE')
+    {
+      if(expression.length == 1)
+        expression = "";
+      else
+        expression = expression.slice(0, expression.length - 1);
+      $('input').val(expression);
+    }
+    else if(key == '=')
+    { 
+      var result = eval(expression);
+      $('input').val(result);
+      expression = result;
+    }
+    else
+    {
+      if($(this).attr('id') == 'mul')
+        expression += '*'
+      else if($(this).attr('id') == 'div')
+        expression += '/';
+      else
+        expression += key;
+      $('input').val(expression);
+    }
+  });
+});
